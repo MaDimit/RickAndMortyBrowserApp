64a8ab0ebddd3179f5fd4c9cf187c3a6388c731b
Maxim Dimitrov <mdimirtov@gmail.com>
2018-06-27 17:36:48 +0300

    Random icon added.

--- app/src/main/java/com/example/maksimdimitrov/rickandmorty/controller/MainActivity.kt
+++ app/src/main/java/com/example/maksimdimitrov/rickandmorty/controller/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.maksimdimitrov.rickandmorty.controller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBar
+import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import com.example.maksimdimitrov.rickandmorty.R
-import com.example.maksimdimitrov.rickandmorty.controller.items.Map
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
